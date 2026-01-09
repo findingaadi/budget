69b420af9420aac3ccc0d807ee10555858392d8c
Adarsha Aryal <adarshaaryal30@gmail.com>
2026-01-09 23:10:23 +0000

    switch cases for category enum selection

--- src/budgettracker/service/Calculation.java
+++ src/budgettracker/service/Calculation.java
@@ -19,6 +19,68 @@ public class Calculation {
         System.out.println("Amount?: ");
         double amt = input.nextDouble();
 
+        System.out.println("Please select the category:");
+        System.out.println("1. Groceries");
+        System.out.println("2. Nepal");
+        System.out.println("3. Home Bills");
+        System.out.println("4. Credit Card");
+        System.out.println("5. Subscription");
+        System.out.println("6. Eating Out");
+        System.out.println("7. Shopping");
+        System.out.println("8. Fuel");
+        System.out.println("9. Miscellaneous");
+        System.out.println("10. Savings");
+
+        int i = input.nextInt();
+        Expense.Category e;
+        switch (i){
+            case 1:
+                e = Expense.Category.Groceries;
+            break;
+
+            case 2:
+                e = Expense.Category.Nepal;
+            break;
+
+            case 3:
+                e = Expense.Category.HomeBills;
+            break;
+
+            case 4:
+                e = Expense.Category.CreditCard;
+            break;
+
+            case 5:
+                e = Expense.Category.Subscription;
+            break;
+
+            case 6:
+                e = Expense.Category.EatingOut;
+            break;
+
+            case 7:
+                e = Expense.Category.Shopping;
+            break;
+
+            case 8:
+                e = Expense.Category.Fuel;
+            break;
+
+            case 9:
+                e = Expense.Category.Miscellaneous;
+            break;
+
+            case 10:
+                e = Expense.Category.Savings;
+            break;
+
+        }
+
+
+
+
+        //
+
         Expense Obj1 = new Expense(LocalDate.now(), Expense.Category.Groceries, desc, amt);
         kharcha.add(Obj1);
         for (Expense expense : kharcha) {
