952fde7cf9e725ac0821b3841d9153f26878b3e3
Adarsha Aryal <adarshaaryal30@gmail.com>
2026-01-14 23:14:20 +0000

    menu added.

--- src/budgettracker/app/Main.java
+++ src/budgettracker/app/Main.java
@@ -6,15 +6,7 @@ import budgettracker.service.Calculation;
 public class Main {
     public static void main(String[] Args){
 
-        System.out.println("Hello");
-
-        Calculation obj1 = new Calculation(); //this object is to intiate the calculations
-        Analysis obj2 = new Analysis(obj1); //takes in the calculation object to do the analysis
-//        obj2.addExpense();
-//        obj2.deleteExpense();
-        obj1.addExpense();
-        obj2.dailyExpense();
-        obj2.monthlyExpense();
-
+        Menu menu = new Menu();
+        menu.menu();
     }
 }
---
+++ src/budgettracker/app/Menu.java
@@ -0,0 +1,32 @@
+package budgettracker.app;
+
+import budgettracker.service.Analysis;
+import budgettracker.service.Calculation;
+
+import java.util.Scanner;
+
+public class Menu {
+    public void menu(){
+        Calculation obj1 = new Calculation(); //this object is to intiate the calculations
+        Analysis obj2 = new Analysis(obj1);//takes in the calculation object to do the analysis
+
+        System.out.println("Welcome!");
+        System.out.println("Please Select from the menu below.");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("1. Add an expense.");
+        System.out.println("2. Delete an expense.");
+        System.out.println("3. View Expense Analysis.");
+
+        int i = input.nextInt();
+        switch (i){
+            case 1->obj1.addExpense();
+            case 2->obj1.deleteExpense();
+        }
+//        obj2.addExpense();
+//        obj2.deleteExpense();
+//        obj1.addExpense();
+//        obj2.dailyExpense();
+//        obj2.monthlyExpense();
+    }
+}
