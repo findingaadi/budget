8a2147f9ab7c9724eea3d0bd7982f1296fb6084e
Adarsha Aryal <adarshaaryal30@gmail.com>
2026-01-19 23:47:56 +0000

    additional error handlers.

--- src/budgettracker/service/Calculation.java
+++ src/budgettracker/service/Calculation.java
@@ -2,12 +2,14 @@ package budgettracker.service;
 
 import budgettracker.domain.Expense;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 
-public class Calculation {
+public class Calculation implements Serializable {
+
     ArrayList<Expense> kharcha = new ArrayList<>();
     Scanner input = new Scanner(System.in);
 
@@ -31,61 +33,72 @@ public class Calculation {
     }
 
     public void addExpense(){
-        System.out.println("Description?: ");
-        String desc = input.nextLine();
+        try {
 
-        System.out.println("Amount?: ");
-        double amt = input.nextDouble();
 
-        System.out.println("Date?");
-        LocalDate date = LocalDate.parse(input.nextLine());
+            System.out.println("Description?: ");
+            String desc = input.nextLine();
 
-        System.out.println("Please select the category:");
-        System.out.println("1. Groceries");
-        System.out.println("2. Nepal");
-        System.out.println("3. Home Bills");
-        System.out.println("4. Credit Card");
-        System.out.println("5. Subscription");
-        System.out.println("6. Eating Out");
-        System.out.println("7. Shopping");
-        System.out.println("8. Fuel");
-        System.out.println("9. Miscellaneous");
-        System.out.println("10. Savings");
+            System.out.println("Amount?: ");
+            double amt = input.nextDouble();
 
-        int i = input.nextInt();
-        Expense.Category e = switch (i) {
-            case 1 -> Expense.Category.Groceries;
-            case 2 -> Expense.Category.Nepal;
-            case 3 -> Expense.Category.HomeBills;
-            case 4 -> Expense.Category.CreditCard;
-            case 5 -> Expense.Category.Subscription;
-            case 6 -> Expense.Category.EatingOut;
-            case 7 -> Expense.Category.Shopping;
-            case 8 -> Expense.Category.Fuel;
-            case 9 -> Expense.Category.Miscellaneous;
-            case 10 -> Expense.Category.Savings;
-            default -> null;
-        };
+            System.out.println("Date?");
+            LocalDate date = LocalDate.parse(input.nextLine());
 
-        Expense Obj1 = new Expense(date, e, desc, amt);
-        Expense Obj2 = new Expense(LocalDate.now(), e, "autoTest", 40.0);
-        Expense Obj3 = new Expense(LocalDate.now(), e, "autoTest", 50.0);
-        Expense Obj4 = new Expense(LocalDate.now(), e, "autoTest", 60.0);
-        Expense Obj5 = new Expense(LocalDate.now(), e, "autoTest", 70.0);
-        kharcha.add(Obj1);
-        kharcha.add(Obj2);
-        kharcha.add(Obj3);
-        kharcha.add(Obj4);
-        kharcha.add(Obj5);
-        displayAllExpense();
+            System.out.println("Please select the category:");
+            System.out.println("1. Groceries");
+            System.out.println("2. Nepal");
+            System.out.println("3. Home Bills");
+            System.out.println("4. Credit Card");
+            System.out.println("5. Subscription");
+            System.out.println("6. Eating Out");
+            System.out.println("7. Shopping");
+            System.out.println("8. Fuel");
+            System.out.println("9. Miscellaneous");
+            System.out.println("10. Savings");
+
+            int i = input.nextInt();
+            Expense.Category e = switch (i) {
+                case 1 -> Expense.Category.Groceries;
+                case 2 -> Expense.Category.Nepal;
+                case 3 -> Expense.Category.HomeBills;
+                case 4 -> Expense.Category.CreditCard;
+                case 5 -> Expense.Category.Subscription;
+                case 6 -> Expense.Category.EatingOut;
+                case 7 -> Expense.Category.Shopping;
+                case 8 -> Expense.Category.Fuel;
+                case 9 -> Expense.Category.Miscellaneous;
+                case 10 -> Expense.Category.Savings;
+                default -> null;
+            };
+
+            Expense Obj1 = new Expense(date, e, desc, amt);
+            Expense Obj2 = new Expense(LocalDate.now(), e, "autoTest", 40.0);
+            Expense Obj3 = new Expense(LocalDate.now(), e, "autoTest", 50.0);
+            Expense Obj4 = new Expense(LocalDate.now(), e, "autoTest", 60.0);
+            Expense Obj5 = new Expense(LocalDate.now(), e, "autoTest", 70.0);
+            kharcha.add(Obj1);
+            kharcha.add(Obj2);
+            kharcha.add(Obj3);
+            kharcha.add(Obj4);
+            kharcha.add(Obj5);
+
+            displayAllExpense();
+        }catch (Exception e){
+            System.out.println("Something went wrong.");
+        }
         }
 
     public void deleteExpense(){
         System.out.println("Which expense would you like to delete? ");
         displayAllExpense();
-
-        int l = input.nextInt();
-        kharcha.remove(l-1);
+        int l =0;
+        try{
+            l = input.nextInt();
+            kharcha.remove(l-1);
+        } catch (Exception e) {
+            System.out.println("Something went wrong!");
+        }
         displayAllExpense();
     }
 
